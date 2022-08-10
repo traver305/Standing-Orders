2bb79fff699d6f22d26cfac35f63349e084da923
Sloboda Dominik <dominik.sloboda@softec.sk>
2022-08-10 10:58:01 +0200

    set amount formController to number type

--- app/src/app/standingOrders/standing-order-form/standing-order-form.component.ts
+++ app/src/app/standingOrders/standing-order-form/standing-order-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { MatFormFieldAppearance } from '@angular/material/form-field';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'pm-standing-order-form',
@@ -12,13 +13,16 @@ export class StandingOrderFormComponent {
 
     appereance: MatFormFieldAppearance ='fill';
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private route: ActivatedRoute) {
+        // route.params.pipe(
+        //     tap(params => console.log('activated params', params))
+        // ).subscribe();
     }
 
     standingOrderForm = new FormGroup({
         name: new FormControl(''),
         iban: new FormControl(''),
-        amount: new FormControl(0),
+        amount: new FormControl<number | null>(null),
         variableSybmol: new FormControl(''),
         constSymbol: new FormControl(''),
         specificSymbol: new FormControl(''),
