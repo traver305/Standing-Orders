9f7ae420eae1b7f45728da0cb06b35649bb498b4
Sloboda Dominik <dominik.sloboda@softec.sk>
2022-08-10 09:53:15 +0200

    type variable appereance to type MatFormFieldAppearance

--- app/src/app/standingOrders/standing-order-form/standing-order-form.component.ts
+++ app/src/app/standingOrders/standing-order-form/standing-order-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { MatFormFieldAppearance } from '@angular/material/form-field';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class StandingOrderFormComponent {
 
-    appereance ='fill';
+    appereance: MatFormFieldAppearance ='fill';
 
     constructor(private router: Router) {
     }
